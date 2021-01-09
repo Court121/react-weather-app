1934ce243d126b4a69f9498c03cfcbec1eeb0611
Courtney Daniels <69318339+Court121@users.noreply.github.com>
2021-01-09 04:23:56 -0500

    added bootstrap & axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,31 @@
 import React,{useState} from "react";
-import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css"
+import axios from "axios";
 
-export default function Weather(){
-    function handleResponse(response){
+
+export default function Weather(props){
+    let [city,setCity]= useState(props.defaultCity);
+    let [weatherData,setWeatherData] = useState({ready:false});
+    function handleResponse(){
+        setWeatherData({
+            ready:true,
+            temperture: response.data.main.temp,
+            icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+            wind: response.data.wind.speed,
+            city: response.data.name,
+            description: response.data.weather[0].description,
+            humidity:response.data.main.humidity,
+            date: new Date(response.data.dt*1000)
+        });
 
     }
-    const apiKey = 
+    
+    function search() {
+        let apiKey = "9e495337e0feec7$ab53c0d9ca024297c";
+     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(url).then(handleResponse);}
+    
      return(
          <div className="Weather">
              <form> 
